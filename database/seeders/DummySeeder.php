f621b52fb3814a77cb85d6ffffe1578fc2ab496e
Mohamed5550 <mohamed.hesham5550@gmail.com>
2022-11-24 20:52:46 +0200

    adding database migrations and seeders

---
+++ database/seeders/DummySeeder.php
@@ -0,0 +1,633 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DummySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::insert([
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "1",
+                "product" => "product-1",
+                "category" => "category-1",
+                "brand" => "brand-1"
+            ],
+            [
+                "id" => "2",
+                "product" => "product-2",
+                "category" => "category-2",
+                "brand" => "brand-2"
+            ],
+            [
+                "id" => "2",
+                "product" => "product-2",
+                "category" => "category-2",
+                "brand" => "brand-2"
+            ],
+            [
+                "id" => "2",
+                "product" => "product-2",
+                "category" => "category-2",
+                "brand" => "brand-2"
+            ],
+            [
+                "id" => "2",
+                "product" => "product-2",
+                "category" => "category-2",
+                "brand" => "brand-2"
+            ],
+            [
+                "id" => "2",
+                "product" => "product-2",
+                "category" => "category-2",
+                "brand" => "brand-2"
+            ],
+            [
+                "id" => "2",
+                "product" => "product-2",
+                "category" => "category-2",
+                "brand" => "brand-2"
+            ],
+            [
+                "id" => "2",
+                "product" => "product-2",
+                "category" => "category-2",
+                "brand" => "brand-2"
+            ],
+            [
+                "id" => "2",
+                "product" => "product-2",
+                "category" => "category-2",
+                "brand" => "brand-2"
+            ],
+            [
+                "id" => "2",
+                "product" => "product-2",
+                "category" => "category-2",
+                "brand" => "brand-2"
+            ],
+            [
+                "id" => "2",
+                "product" => "product-2",
+                "category" => "category-2",
+                "brand" => "brand-2"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+            [
+                "id" => "3",
+                "product" => "product-3",
+                "category" => "category-3",
+                "brand" => "brand-3"
+            ],
+        ]);
+    }
+}
