254ee11918113945677e33d93ea8f1311d350b4c
Mohamed5550 <mohamed.hesham5550@gmail.com>
2022-11-25 14:01:28 +0200

    completing blade missing assets function

--- resources/views/components/general/head.blade.php
+++ resources/views/components/general/head.blade.php
@@ -3,7 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
     <title>{{ $title }}</title>
 
     {{-- For CSRF  --}}
@@ -11,5 +11,5 @@
 
     {{-- For search js submit --}}
     <meta name="search-route" content="{{ route('products.search', true) }}"/>
-    <script src="js/main.js"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 </head>
